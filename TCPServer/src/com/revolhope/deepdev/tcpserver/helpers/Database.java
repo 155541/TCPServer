294bc0ae69a73185ff34b843fb593206ea06abfe
splanes <155541.clot@fje.edu>
2018-10-24 23:19:33 +0200

    progress 3

--- TCPServer/src/com/revolhope/deepdev/tcpserver/helpers/Database.java
+++ TCPServer/src/com/revolhope/deepdev/tcpserver/helpers/Database.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.revolhope.deepdev.tcplibrary.model.Device;
+import com.revolhope.deepdev.tcplibrary.model.Token;
 
 public class Database {
 	
@@ -16,6 +17,7 @@ public class Database {
 	private static final String DB_NAME = "FTP_Server";
 	private static final String DB_USR = "ftp_server";
 	private static final String DB_PWD = "b1ql83dD$;Reka!lPOd80_234";
+	private static final String DB_PARAMS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	
 	private static Database INSTANCE;
 	private static Connection conn;
@@ -65,7 +67,7 @@ public class Database {
 		
 		if (affected == 1 && result.first())
 		{
-			dev.setId(result.getLong(0));
+			dev.setId(result.getLong(1));
 			ps.close();
 			conn.close();
 			return dev;
@@ -75,6 +77,88 @@ public class Database {
 			ps.close();
 			conn.close();
 			return null;
+		}
+	}
+	
+	/***
+	 * Method to insert new Token into database
+	 * @param token Object to be stored
+	 * @throws SQLException
+	 */
+	public void insertToken(Token token) throws SQLException
+	{
+		openConnection();
+		String query = "INSERT INTO TOKEN(DEVICE_ID, _TOKEN, EXPIRATION_DATE) VALUES(?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ps.setLong(1, token.getDeviceId());
+		ps.setString(2, token.toString());
+		ps.setLong(3, token.getExpirationDate());
+		
+		int affected = ps.executeUpdate();
+		
+		if (affected != 1)
+		{
+			ps.close();
+			conn.close();
+			throw new SQLException("Error in sql method. Affected rows != 1 before insert");
+		}
+	}
+	
+	/***
+	 * Method to update a Token
+	 * @param token Object to be updated
+	 * @throws SQLException
+	 */
+	public void updateToken(Token token) throws SQLException
+	{
+		openConnection();
+		String query = "UPDATE FROM TOKEN SET _TOKEN = ?, EXPIRATION_DATE = ? WHERE DEVICE_ID = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ps.setString(1, token.toString());
+		ps.setLong(2, token.getExpirationDate());
+		ps.setLong(3, token.getDeviceId());
+		
+		int affected = ps.executeUpdate();
+		
+		if (affected != 1)
+		{
+			ps.close();
+			conn.close();
+			throw new SQLException("Error in sql method. Affected rows != 1 before update");
+		}
+	}
+	
+	/***
+	 * Method to select a Token given it's device id
+	 * @param deviceId Long identifier of the token requested
+	 * @return Token object
+	 * @throws SQLException
+	 */
+	public Token selectToken(long deviceId) throws SQLException
+	{
+		openConnection();
+		String query = "SELECT _TOKEN, EXPIRATION_DATE FROM TOKEN WHERE DEVICE_ID = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ps.setLong(1, deviceId);
+		ResultSet result = ps.executeQuery();
+		if (result.first())
+		{
+			String token = result.getString(1);
+			long expiration = result.getLong(2);
+			
+			ps.close();
+			conn.close();
+			
+			return new Token(deviceId, token, expiration);
+		}
+		else
+		{
+			ps.close();
+			conn.close();
+			throw new SQLException("Token associated to given deviceId not found in database..");
 		}
 	}
 	
@@ -95,10 +179,10 @@ public class Database {
 		if (result.first())
 		{
 			Device dev = new Device();
-			dev.setId(result.getLong(0));
-			dev.setName(result.getString(1));
-			dev.setMacAddress(result.getString(2));
-			dev.setCreatedDate(result.getLong(3));
+			dev.setId(result.getLong(1));
+			dev.setName(result.getString(2));
+			dev.setMacAddress(result.getString(3));
+			dev.setCreatedDate(result.getLong(4));
 			
 			ps.close();
 			conn.close();
@@ -113,11 +197,35 @@ public class Database {
 	}
 	
 	/***
+	 * Method to verify if token given is valid or not
+	 * @param token Object to be verified
+	 * @return True if token is equals than stored in database, false otherwise
+	 * @throws SQLException
+	 */
+	public boolean verifyToken(Token token) throws SQLException
+	{
+		openConnection();
+		String query = "SELECT * FROM TOKEN WHERE DEVICE_ID = ? AND _TOKEN = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ps.setLong(1, token.getDeviceId());
+		ps.setString(2, token.toString());
+		
+		ResultSet result = ps.executeQuery();
+		
+		boolean verified = result.first() && !result.next();
+		ps.close();
+		conn.close();
+		
+		return verified;
+	}
+	
+	/***
 	 * Open connection for the URL, user and password set
 	 * @throws SQLException
 	 */
 	private static void openConnection() throws SQLException
 	{
-		conn = DriverManager.getConnection("jdbc:mysql://"+DB_URL+DB_NAME, DB_USR, DB_PWD);
+		conn = DriverManager.getConnection("jdbc:mysql://"+DB_URL+DB_NAME+DB_PARAMS, DB_USR, DB_PWD);
 	}
 }
--- TCPServer/src/com/revolhope/deepdev/tcpserver/helpers/Toolkit.java
+++ TCPServer/src/com/revolhope/deepdev/tcpserver/helpers/Toolkit.java
@@ -1,44 +1,45 @@
 package com.revolhope.deepdev.tcpserver.helpers;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+
+import java.util.ArrayList;
 
 import com.revolhope.deepdev.tcplibrary.model.Device;
 
 public class Toolkit 
 {
-	private static Map<Long, String> mapTokens = new HashMap<>();
+	ArrayList<Device> connectedDevices = new ArrayList<>();
 	
 	/***
-	 * Private constructor
+	 * 
+	 * @param dev
 	 */
-	private Toolkit() {}
-	
-	/***
-	 * Method to generate and get a new Token for given Device
-	 * @param device Device to assign the new generated token
-	 * @return String representing the new token
-	 */
-	public static String generateToken(Device device)
+	public void addConnectedDevice(Device dev)
 	{
-		String token = UUID.randomUUID().toString().replace("-", "");
-		mapTokens.put(device.getId(), token);
-		return token;
+		removeConnectedDevice(dev);
+		connectedDevices.add(0, dev);
 	}
 	
 	/***
-	 * Method to verify if token given is correct
-	 * @param dev Device who send the token
-	 * @param token Token (String) to verify
-	 * @return True if token given is equals to the token stored
+	 * 
+	 * @param dev
 	 */
-	public static boolean verifyToken(Device dev, String token)
+	public void removeConnectedDevice(Device dev)
 	{
-		if (mapTokens.containsKey(dev.getId()))
+		for (Device d : connectedDevices)
 		{
-			return mapTokens.get(dev.getId()).equals(token);
+			if (d.getId() == dev.getId())
+			{
+				connectedDevices.remove(d);
+			}
 		}
-		return false;
+	}
+	
+	/***
+	 * 
+	 * @return
+	 */
+	public ArrayList<Device> getConnectedDevices()
+	{
+		return this.connectedDevices;
 	}
 }
